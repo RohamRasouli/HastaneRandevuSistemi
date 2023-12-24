e92f1175aa37504be4464b8004cd5f1c7109a355
Roham Kerahroudi <roham.kerahroudi@ogr.sakarya.edu.tr>
2023-12-24 17:30:18 +0300

    ASP.NET MVC Project

---
+++ HastaneRandevuSistemi/HastaneRandevuSistemi/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult LogIn() 
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
